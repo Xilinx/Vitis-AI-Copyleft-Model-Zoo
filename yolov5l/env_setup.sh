0cbae6eb5f75993e63b37112063794d07a47a5f3
wangxd <wangxd@xilinx.com>
2023-06-01 15:20:10 +0800

    update yolo code

--- yolov5l/env_setup.sh
+++
@@ -1,5 +0,0 @@
-ROOT_DIR=${PWD}
-echo "Install all the python dependencies using pip"
-pip install --trusted-host xcdpython.xilinx.com -r requirements_ptq.txt
-
-cd ${ROOT_DIR}
--- yolov8m/env_setup.sh
+++
@@ -1,4 +0,0 @@
-echo "Install all the python dependencies using pip"
-pip install --trusted-host xcdpython.xilinx.com -r requirements_ptq.txt
-cd code/
-python setup.py develop
